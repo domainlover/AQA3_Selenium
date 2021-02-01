264595b9c8e235b3b54b69b232e190549d515a6a
Dmitry Urin <domainlover@yahoo.com>
2021-02-01 05:44:26 +0200

    Initial commit

---
+++ src/test/java/ru/netology/web/OrderTest.java
@@ -0,0 +1,83 @@
+package ru.netology.web;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Util {
+    public enum OS {
+        WINDOWS, LINUX, MAC, SOLARIS
+    };// Operating systems.
+
+    private static OS os = null;
+
+    public static OS getOS() {
+        if (os == null) {
+            String operSys = System.getProperty("os.name").toLowerCase();
+            if (operSys.contains("win")) {
+                os = OS.WINDOWS;
+            } else if (operSys.contains("nix") || operSys.contains("nux")
+                    || operSys.contains("aix")) {
+                os = OS.LINUX;
+            } else if (operSys.contains("mac")) {
+                os = OS.MAC;
+            } else if (operSys.contains("sunos")) {
+                os = OS.SOLARIS;
+            }
+        }
+        return os;
+    }
+}
+
+class OrderTest {
+    
+
+    @BeforeAll
+    static void setUpAll() {
+        switch (Util.getOS()) {
+            case WINDOWS:
+                System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+                break;
+            case LINUX:
+                System.setProperty("webdriver.chrome.driver", "chromedriver");
+                break;
+        }
+    }
+
+    private WebDriver driver;
+
+    @BeforeEach
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void validNameAndNumber() {
+        driver.get("http://localhost:9999");
+        WebElement form = driver.findElement(By.className("form"));
+        form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Урин Дмитрий");
+        form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79270000000");
+        form.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
+}
+
